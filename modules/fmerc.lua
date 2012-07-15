8184126a7e6e12c01c6fbfea52f425a7435f15cf
niczkx <f.fallen45@gmail.com>
2012-07-15 07:35:06 +0300

    trolol

--- modules/fmerc.lua
+++ modules/fmerc.lua
@@ -1,2 +1,7 @@
 -- this file is loaded after all modules are loaded and initialized
 -- you can place any custom user code here
+
+-- map editor modules
+g_modules.autoLoadModules(999);
+g_modules.ensureModuleLoaded("mapeditor")
+
