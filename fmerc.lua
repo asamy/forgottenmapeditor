c407371b7946c7e14440af6bb638001f5e6fab13
Eduardo Bart <edub4rt@gmail.com>
2012-07-28 02:13:35 -0300

    update

--- fmerc.lua
+++ fmerc.lua
@@ -2,6 +2,10 @@
 -- you can place any custom user code here
 
 -- map editor modules
+g_window.setVerticalSync(false)
+g_app.setForegroundPaneMaxFps(24)
+
 g_things.loadOtb('/data/forgotten-items.otb')
 g_map.loadOtbm('/data/forgotten.otbm')
-mapWidget:setCameraPosition({y=498,x=207,z=7})
+--g_map.loadOtcm('/data/minimapfull.otcm')
+mapWidget:setCameraPosition({y=143,x=100,z=7})
