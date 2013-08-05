3a37ca491a55b630aa35ceee09d99853864d7c99
BrunoDCC <comedinha>
2013-08-05 17:44:36 -0300

    Save houses and spawns fixes new change on uieditablemap Change text 'Test' to 'Forgotten Map Editor'

--- modules/mapeditor_startup/startup.lua
+++ modules/mapeditor_startup/startup.lua
@@ -7,7 +7,7 @@
 OTB_FILE   = "/data/materials/items.otb"
 XML_FILE   = "/data/materials/items.xml"
 MON_FILE   = "/data/materials/monster/monsters.xml"
-NPC_FOLDER = "/data/materials/npcs"
+NPC_FOLDER = "/data/materials/npc"
 OTBM_FILE  = "/data/materials/map.otbm"
 DAT_FILE   = "/data/materials/Tibia.dat"
 SPR_FILE   = "/data/materials/Tibia.spr"
@@ -30,8 +30,9 @@ function startup()
   g_things.loadXml(XML_FILE)
   -- load up monsters
   --g_creatures.loadMonsters(MON_FILE)
-  -- uncomment this if  you wanna load NPCs
-  -- g_map.loadNpcs(NPC_FOLDER)
+  -- load up npcs (uncomment this if you wanna load NPCs)
+ -- g_creatures.loadNpcs(NPC_FOLDER)
+  -- load up otbm
   -- g_map.loadOtbm(OTBM_FILE)
 end
 
