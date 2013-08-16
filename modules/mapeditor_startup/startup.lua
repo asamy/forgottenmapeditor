f0deb1374a0fc46821b70cd7de540acc6370a6e9
BrunoDCC <comedinhass@gmail.com>
2013-08-16 17:16:24 -0300

    New fileloader with folders

--- modules/mapeditor_startup/startup.lua
+++ modules/mapeditor_startup/startup.lua
@@ -3,37 +3,74 @@
 --  Therefore you'll need to download them as a "third-party" from
 --  various ditros and Tibia versions.
 
-
-OTB_FILE   = "/data/materials/items.otb"
-XML_FILE   = "/data/materials/items.xml"
-MON_FILE   = "/data/materials/monster/monsters.xml"
-NPC_FOLDER = "/data/materials/npc"
-OTBM_FILE  = "/data/materials/map.otbm"
-DAT_FILE   = "/data/materials/Tibia.dat"
-SPR_FILE   = "/data/materials/Tibia.spr"
-VERSION    = 870  -- Most important for loading Tibia files correctly.
+VERSION        = 870  -- Most important for loading Tibia files correctly.
+VERSION_FOLDER = "/data/materials/"..VERSION.."/"
+OTB_FILE       = "/data/materials/"..VERSION.."/items.otb"
+XML_FILE       = "/data/materials/"..VERSION.."/items.xml"
+MON_FILE       = "/data/materials/"..VERSION.."/monster/monsters.xml"
+NPC_FOLDER     = "/data/materials/"..VERSION.."/npc"
+DAT_FILE       = "/data/materials/"..VERSION.."/Tibia.dat"
+SPR_FILE       = "/data/materials/"..VERSION.."/Tibia.spr"
 
 -- Nothing beyond here is useful to people who can't code
 function startup()
-  print("Starting up...")
+  print("-> Loading startup files...")
   -- All of the functions below throw exceptions on failure
   -- not in terms of terminaing the applications, though.
-  g_game.setClientVersion(VERSION)
-  g_game.setProtocolVersion(VERSION)
-  -- Load up dat.
-  g_things.loadDat(DAT_FILE)
-  -- Load up SPR.
-  g_sprites.loadSpr(SPR_FILE)
-  -- Load up OTB
-  g_things.loadOtb(OTB_FILE)
-  -- Load up XML
-  g_things.loadXml(XML_FILE)
-  -- load up monsters
-  --g_creatures.loadMonsters(MON_FILE)
-  -- load up npcs (uncomment this if you wanna load NPCs)
- -- g_creatures.loadNpcs(NPC_FOLDER)
-  -- load up otbm
-  -- g_map.loadOtbm(OTBM_FILE)
+  if g_game.setClientVersion(VERSION) then
+    print("Error after load "..VERSION.." version.")
+  else
+    g_game.setProtocolVersion(VERSION)
+    print("--> Loading with version "..VERSION.."")
+    if g_resources.directoryExists(VERSION_FOLDER) then
+      print("---> "..VERSION.." Loaded")
+      -- Load up dat.
+      print("--> Loading dat...")
+      if g_resources.fileExists(DAT_FILE) then
+        g_things.loadDat(DAT_FILE)
+      else
+        print("---> Error with load dat. '"..DAT_FILE.."' File not found")
+      end
+      -- Load up SPR.
+      print("--> Loading spr...")
+      if g_resources.fileExists(SPR_FILE) then
+        g_sprites.loadSpr(SPR_FILE)
+      else
+        print("---> Error with load spr. '"..SPR_FILE.."' File not found")
+      end
+      -- Load up OTB
+      print("--> Loading OTB...")
+      if g_resources.fileExists(OTB_FILE) then
+        g_things.loadOtb(OTB_FILE)
+      else
+        print("---> Error with load OTB. '"..OTB_FILE.."' File not found")
+      end
+      -- Load up XML
+      print("--> Loading XML...")
+      if g_resources.fileExists(XML_FILE) then
+        g_things.loadXml(XML_FILE)
+      else
+        print("---> Error with load XML. '"..XML_FILE.."' File not found")
+      end
+      -- load up monsters
+      print("--> Loading monsters...")
+      if g_resources.fileExists(MON_FILE) then
+        g_creatures.loadMonsters(MON_FILE)
+      else
+        print("---> Error with load monsters. '"..MON_FILE.."' File not found")
+      end
+      -- load up npcs (uncomment this if you wanna load NPCs)]
+      print("--> Loading NPCs...")
+      if g_resources.directoryExists(NPC_FOLDER) then
+        g_creatures.loadNpcs(NPC_FOLDER)
+      else
+        print("---> Error with load NPCs. '"..NPC_FOLDER.."' Folder not found")
+      end
+    else
+      print("---> Folder "..VERSION.." not found '"..VERSION_FOLDER.."'")
+      return
+    end
+  end
 end
 
 function shutdown()
