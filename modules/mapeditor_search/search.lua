a79a8b63b2b0ad97a9c5e71ec5affff5c6ac6c8c
Ahmed Samy <f.fallen45@gmail.com>
2013-09-02 17:54:38 +0000

    Search module: use of c++ g_map.findItemsById

--- modules/mapeditor_search/search.lua
+++ modules/mapeditor_search/search.lua
@@ -112,32 +112,12 @@ function SearchModule.searchOnMap()
   SearchModule.hideWindow()
   SearchModule.showWidget()
 
-  -- I hate pyramids... sorry for it :-)
-  function f()
-    local mapSize = g_map.getSize()
-    for x = 0, mapSize.width do
-      for y = 0, mapSize.height do
-        for z = 0, 15 do
-          local tile = g_map.getTile({x = x, y = y, z = z})
-          if tile then
-            local things = tile:getThings()
-            for i = 1, #things do
-              if things[i]:getId() == focus.itemId then
-                local widget = g_ui.createWidget('SearchOnMapResult', foundItemsList)
-                widget:setText(things[i]:getName() .. ' (' .. x .. ':' .. y .. ':' .. z ..')')
-                g_mouse.bindPress(widget, function() mapWidget:setCameraPosition({x = x, y = y, z = z}) end, MouseLeftButton)
-                results = results + 1
-              end
-              if results >= 500 then
-                return true
-              end
-            end
-          end
-        end
-      end
-    end
+  local items = g_map.findItemsById(focus.itemId, 500)
+  for pos, item in pairs(items) do
+    local widget = g_ui.createWidget('SearchOnMapResult', foundItemsList)
+    widget:setText(item:getName() .. ' (' .. pos.x .. ':' .. pos.y .. ':' .. pos.z ..')')
+    g_mouse.bindPress(widget, function() mapWidget:setCameraPosition({x = pos.x, y = pos.y, z = pos.z}) end, MouseLeftButton)
   end
-  f()
 end
 
 function SearchModule.toggleWidget()
